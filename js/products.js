64a423545fee6e6f778b8dd40e99e04d5be83804
Anasta07 <ihorpukhalo@gmail.com>
2021-03-31 09:29:23 +0000

    end

--- js/products.js
+++ js/products.js
@@ -4,49 +4,49 @@
         "id": "1",
         "image": "areca-palm.png",
         "title": "Areca Palm",
-        "price": "22,00"
+        "price": 22.00 
     },
     {
         "id": "2",
         "image": "monstera-obliqua.jpg",
         "title": "Monstera Obliqua",
-        "price": "45,00"
+        "price": 45.00
     },
     {
         "id": "3",
         "image": "floral-moss.png",
         "title": "Floral Moss",
-        "price": "45,00"
+        "price": 45.00
     },
     {
         "id": "4",
         "image": "monstera-obliqua.jpg",
         "title": "Monstera Obliqua",
-        "price": "45,00"
+        "price": 45.00
     },
     {
         "id": "5",
         "image": "haworthia-venosa.png",
         "title": "Haworthia Venosa",
-        "price": "48,00"
+        "price": 48.00
     },
     {
         "id": "6",
         "image": "silver-torch.png",
         "title": "Silver Torch",
-        "price": "32,00"
+        "price": 32.00 
     },
     {
         "id": "7",
         "image": "notocactus-roseoluteus.png",
         "title": "Notocactus Roseoluteus",
-        "price": "33,00"
+        "price": 33.00
     },
     {
         "id": "8",
         "image": "graptosedum-darley.png",
         "title": "Graptosedum Darley",
-        "price": "32,00"
+        "price": 32.00
     }
 ]
  `;
@@ -64,7 +64,7 @@ function renderProducts(products, sortOrder = 'ascending') {
                         <img src="img/${product.image}"  alt="${product.title}">
                         <div class="card-body">
                             <h5>${product.title}</h5>
-                            <p>${product.price}</p>
+                            <p>$${product.price}.00USD</p>
                             <a href="#contacts" class="btn btn-light">LEARN MORE</a>
                         </div>
                     </div>`
